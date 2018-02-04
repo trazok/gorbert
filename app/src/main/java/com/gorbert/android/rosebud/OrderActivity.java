f77f0a5b46a62715ab64d43f59786113c442263a
trazok <walkermm@appstate.edu>
2018-02-03 21:32:22 -0500

    tutorial servlet functional

---
+++ app/src/main/java/com/gorbert/android/rosebud/OrderActivity.java
@@ -0,0 +1,17 @@
+package com.gorbert.android.rosebud;
+
+import android.app.Activity;
+import android.widget.Button;
+
+/**
+ * Created by dell on 2/3/2018.
+ */
+
+public class OrderActivity extends Activity
+{
+    private Button mOrderButton;
+
+
+    
+
+}
