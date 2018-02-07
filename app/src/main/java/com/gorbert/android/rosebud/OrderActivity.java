a2cc1e1257062bf820a551b92664cfb5c5afe47f
trazok <walkermm@appstate.edu>
2018-02-07 13:29:50 -0500

    shit layout complete. need to finish servlet

--- app/src/main/java/com/gorbert/android/rosebud/OrderActivity.java
+++ app/src/main/java/com/gorbert/android/rosebud/OrderActivity.java
@@ -1,17 +1,42 @@
 package com.gorbert.android.rosebud;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by dell on 2/3/2018.
  */
 
-public class OrderActivity extends Activity
+public class OrderActivity extends AppCompatActivity
 {
     private Button mOrderButton;
 
     @Override
-    OnCreate
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_order);
+
+        mOrderButton = (Button) findViewById(R.id.order_button);
+        mOrderButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                placeOrder();
+
+            }
+        });
+    }
+
+    public void placeOrder()
+    {
+        //contact servlet to log order
+        Toast.makeText(this, R.string.order_toast, Toast.LENGTH_SHORT).show();
+    }
 }
