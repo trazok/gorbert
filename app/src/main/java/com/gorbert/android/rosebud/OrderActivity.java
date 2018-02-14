af168ccf37a524ad0558317be6a8e0b6484a3f46
trazok <walkermm@appstate.edu>
2018-02-14 14:24:36 -0500

    still http/emulator troubleshooting

--- app/src/main/java/com/gorbert/android/rosebud/OrderActivity.java
+++ app/src/main/java/com/gorbert/android/rosebud/OrderActivity.java
@@ -61,10 +61,10 @@ public class OrderActivity extends AppCompatActivity
     private class PlaceOrderTask extends AsyncTask<String, Void, Integer>
     {
         private String mOrder;
+        mOrder = "tea";
 
-        System.
         System.setProperty("http.proxyHost", "127.0.0.1");
-	    System.setProperty("https.proxyHost", "127.0.0.1");
+        System.setProperty("https.proxyHost", "127.0.0.1");
 	    System.setProperty("http.proxyPort", "8888");
 	    System.setProperty("https.proxyPort", "8888");
 
