0a482b0f969376f8059fc88c44868e9ed12e1372
trazok <walkermm@appstate.edu>
2018-02-14 11:13:59 -0500

    retrying post

--- app/src/main/java/com/gorbert/android/rosebud/OrderActivity.java
+++ app/src/main/java/com/gorbert/android/rosebud/OrderActivity.java
@@ -12,6 +12,9 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by dell on 2/3/2018.
@@ -50,17 +53,27 @@ public class OrderActivity extends AppCompatActivity
 
     public void placeOrder() throws Exception
     {
-        //contact servlet to log order
-        String order = "black coffee";
-        String url = "[192.168.1.4:9999/receive_order/order]";
-        //byte[] postDataBytes = order.getBytes();
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        
+        URL url = new URL("http://example.net/new-message.php");
 
-        //con.setRequestMethod("POST");
-        //con.setDoOutput(true);
-        //con.getOutputStream().write(postDataBytes);
+        Map<String,Object> params = new LinkedHashMap<>();
+        params.put("order", "Black Coffee");
+
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String,Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(postDataBytes);
+
 
         //if (response == con.HTTP_OK)
         Toast.makeText(this, R.string.order_toast, Toast.LENGTH_SHORT).show();
