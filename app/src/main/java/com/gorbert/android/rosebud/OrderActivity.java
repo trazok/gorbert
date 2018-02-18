40726c6e64ad2f018e2f5cf994d7eb3a80ffb3bb
trazok <walkermm@appstate.edu>
2018-02-18 13:45:34 -0500

    simple order logger complete

--- app/src/main/java/com/gorbert/android/rosebud/OrderActivity.java
+++ app/src/main/java/com/gorbert/android/rosebud/OrderActivity.java
@@ -91,7 +91,7 @@ public class OrderActivity extends AppCompatActivity
             URL url = null;
             try
             {
-                url = new URL("http://192.168.1.6:9999/receive_order/order");
+                url = new URL("http://192.168.1.6:9999/receive_order/order"); //dear god, dont forget the http
             }
             catch (MalformedURLException e)
             {
@@ -136,16 +136,18 @@ public class OrderActivity extends AppCompatActivity
                 e.printStackTrace();
             }
 
+            Integer code = 0;
             try
             {
                 InputStream response = connection.getInputStream();
+                code = response.read();
             } catch (IOException e)
             {
                 e.printStackTrace();
             }
 
 
-            return 0;
+            return code;
         }
 
 
