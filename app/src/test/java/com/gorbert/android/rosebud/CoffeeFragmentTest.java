9ecfffaef85a12192a0a756b69b2d19b9f638bd0
trazok <walkermm@appstate.edu>
2018-02-27 13:12:46 -0500

    listing tests

---
+++ app/src/test/java/com/gorbert/android/rosebud/CoffeeFragmentTest.java
@@ -0,0 +1,21 @@
+package com.gorbert.android.rosebud;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dell on 2/27/2018.
+ */
+public class CoffeeFragmentTest
+{
+    //test string order
+    //test pricing
+    //test drink array access
+    //test tea array access
+    //test tea pricing
+    //test tea string order
+    //test database accesses
+    //test add/remove to cart
+    //test add/remove favorites
+    //test server order submission
+    //
+}
