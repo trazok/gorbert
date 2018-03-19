e49c1c8450cc0ac3ff933cfc35840a2b314eaeb8
trazok <trazok@gmail.com>
2018-03-19 17:14:21 -0400

    debugging...

--- app/src/main/java/com/sunrise/android/risingsun/CoffeeListFragment.java
+++ app/src/main/java/com/sunrise/android/risingsun/CoffeeListFragment.java
@@ -38,7 +38,7 @@ public class CoffeeListFragment extends Fragment
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
 
-        
+
     }
 
 
@@ -46,6 +46,7 @@ public class CoffeeListFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
+        
         View view = inflater.inflate(R.layout.fragment_coffee_list, container, false);
 
         mCoffeeRecyclerView = (RecyclerView) view
