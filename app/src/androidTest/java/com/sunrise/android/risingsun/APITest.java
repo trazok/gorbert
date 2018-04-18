fe2295fff3129990b3b0cd0295debce624ce518b
trazok <trazok@gmail.com>
2018-04-18 09:33:24 -0400

    'failing' passing tests

--- app/src/androidTest/java/com/sunrise/android/risingsun/APITest.java
+++ app/src/androidTest/java/com/sunrise/android/risingsun/APITest.java
@@ -19,6 +19,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -26,33 +27,51 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class CartActivityLaunchTest {
+public class APITest {
 
     @Rule
     public ActivityTestRule<CoffeeListActivity> mActivityTestRule = new ActivityTestRule<>(CoffeeListActivity.class);
 
     @Test
-    public void cartActivityLaunchTest() {
+    public void aPITest() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.coffee_recycler_view),
+                        childAtPosition(
+                                withId(R.id.fragment_container),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
         ViewInteraction actionMenuItemView = onView(
-                allOf(withId(R.id.cart_menu), withContentDescription("Shopping Cart"),
+                allOf(withId(R.id.favorited_menu), withContentDescription("Favorites"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(R.id.action_bar),
                                         1),
-                                2),
+                                1),
                         isDisplayed()));
         actionMenuItemView.perform(click());
 
-        ViewInteraction viewGroup = onView(
-                allOf(childAtPosition(
-                        allOf(withId(R.id.fragment_container),
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0)),
-                        0),
+        ViewInteraction frameLayout = onView(
+                allOf(withId(R.id.fragment_container),
+                        childAtPosition(
+                                allOf(withId(android.R.id.content),
+                                        childAtPosition(
+                                                withId(R.id.decor_content_parent),
+                                                1)),
+                                0),
                         isDisplayed()));
-        viewGroup.check(matches(isDisplayed()));
-
+        frameLayout.check(matches(isDisplayed()));
 
     }
 
@@ -64,8 +83,6 @@ public class CartActivityLaunchTest {
             public void describeTo(Description description) {
                 description.appendText("Child at position " + position + " in parent ");
                 parentMatcher.describeTo(description);
-
-
             }
 
             @Override
