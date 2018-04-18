b74602a8b9e905aa728b8697ae0957838912ebac
trazok <trazok@gmail.com>
2018-04-17 23:53:01 -0400

    more UI test fun

--- app/src/androidTest/java/com/sunrise/android/risingsun/UpdateOnRemoveTest.java
+++ app/src/androidTest/java/com/sunrise/android/risingsun/UpdateOnRemoveTest.java
@@ -1,7 +1,6 @@
 package com.sunrise.android.risingsun;
 
 
-import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -13,17 +12,12 @@ import android.view.ViewParent;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
-import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -33,82 +27,50 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class PersistentDataDisplayTest {
+public class UpdateOnRemoveTest {
 
     @Rule
     public ActivityTestRule<CoffeeListActivity> mActivityTestRule = new ActivityTestRule<>(CoffeeListActivity.class);
 
     @Test
-    public void persistentDataDisplayTest() {
+    public void updateOnRemoveTest() {
         ViewInteraction recyclerView = onView(
                 allOf(withId(R.id.coffee_recycler_view),
                         childAtPosition(
                                 withId(R.id.fragment_container),
                                 0)));
-        recyclerView.perform(actionOnItemAtPosition(6, click()));
+        recyclerView.perform(actionOnItemAtPosition(5, click()));
 
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.instructions_edit_text),
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.cart_menu), withContentDescription("Shopping Cart"),
                         childAtPosition(
-                                withParent(withId(R.id.coffee_view_pager)),
-                                15),
-                        isDisplayed()));
-        appCompatEditText.perform(click());
-
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.instructions_edit_text),
-                        childAtPosition(
-                                withParent(withId(R.id.coffee_view_pager)),
-                                15),
-                        isDisplayed()));
-        appCompatEditText2.perform(replaceText("wiggle"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.instructions_edit_text), withText("wiggle"),
-                        childAtPosition(
-                                withParent(withId(R.id.coffee_view_pager)),
-                                15),
-                        isDisplayed()));
-        appCompatEditText3.perform(pressImeActionButton());
-
-        ViewInteraction appCompatSpinner = onView(
-                allOf(withId(R.id.caramel_shots_spinner),
-                        childAtPosition(
-                                withParent(withId(R.id.coffee_view_pager)),
-                                3),
-                        isDisplayed()));
-        appCompatSpinner.perform(click());
-
-        ViewInteraction appCompatSpinner2 = onView(
-                allOf(withId(R.id.hazelnut_shots_spinner),
-                        childAtPosition(
-                                withParent(withId(R.id.coffee_view_pager)),
-                                9),
-                        isDisplayed()));
-        appCompatSpinner2.perform(click());
-
-        DataInteraction appCompatTextView = onData(anything())
-                .inAdapterView(childAtPosition(
-                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
-                        0))
-                .atPosition(3);
-        appCompatTextView.perform(click());
-
-        ViewInteraction appCompatImageButton = onView(
-                allOf(withId(R.id.save_as_favorite_button),
-                        childAtPosition(
-                                allOf(withId(R.id.linearLayout),
-                                        childAtPosition(
-                                                withClassName(is("android.support.constraint.ConstraintLayout")),
-                                                16)),
+                                childAtPosition(
+                                        withId(R.id.action_bar),
+                                        2),
                                 0),
                         isDisplayed()));
-        appCompatImageButton.perform(click());
+        actionMenuItemView.perform(click());
+
+        ViewInteraction actionMenuItemView2 = onView(
+                allOf(withId(R.id.coffee_menu), withContentDescription("Menu"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView2.perform(click());
+
+        ViewInteraction recyclerView2 = onView(
+                allOf(withId(R.id.coffee_recycler_view),
+                        childAtPosition(
+                                withId(R.id.fragment_container),
+                                0)));
+        recyclerView2.perform(actionOnItemAtPosition(2, click()));
 
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.add_to_cart_button), withText("Add to cart"),
@@ -121,7 +83,18 @@ public class PersistentDataDisplayTest {
                         isDisplayed()));
         appCompatButton.perform(click());
 
-        ViewInteraction actionMenuItemView = onView(
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.save_as_favorite_button),
+                        childAtPosition(
+                                allOf(withId(R.id.linearLayout),
+                                        childAtPosition(
+                                                withClassName(is("android.support.constraint.ConstraintLayout")),
+                                                16)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction actionMenuItemView3 = onView(
                 allOf(withId(R.id.cart_menu), withContentDescription("Shopping Cart"),
                         childAtPosition(
                                 childAtPosition(
@@ -129,33 +102,14 @@ public class PersistentDataDisplayTest {
                                         2),
                                 0),
                         isDisplayed()));
-        actionMenuItemView.perform(click());
+        actionMenuItemView3.perform(click());
 
-        ViewInteraction recyclerView2 = onView(
+        ViewInteraction recyclerView3 = onView(
                 allOf(withId(R.id.cart_recycler_view),
                         childAtPosition(
                                 withClassName(is("android.support.constraint.ConstraintLayout")),
                                 0)));
-        recyclerView2.perform(actionOnItemAtPosition(0, click()));
-
-        ViewInteraction editText = onView(
-                allOf(withId(R.id.instructions_edit_text), withText("wiggle"),
-                        childAtPosition(
-                                withParent(withId(R.id.cart_view_pager)),
-                                15),
-                        isDisplayed()));
-        editText.check(matches(withText("wiggle")));
-
-        ViewInteraction textView = onView(
-                allOf(withId(android.R.id.text1), withText("3"),
-                        childAtPosition(
-                                allOf(withId(R.id.hazelnut_shots_spinner),
-                                        childAtPosition(
-                                                IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
-                                                9)),
-                                0),
-                        isDisplayed()));
-        textView.check(matches(withText("3")));
+        recyclerView3.perform(actionOnItemAtPosition(0, click()));
 
         ViewInteraction appCompatButton2 = onView(
                 allOf(withId(R.id.remove_button), withText("Remove"),
@@ -176,17 +130,7 @@ public class PersistentDataDisplayTest {
                         isDisplayed()));
         appCompatImageButton2.perform(click());
 
-        ViewInteraction textView2 = onView(
-                allOf(withId(R.id.order_total_view), withText("$0.00"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.fragment_container),
-                                        0),
-                                1),
-                        isDisplayed()));
-        textView2.check(matches(withText("$0.00")));
-
-        ViewInteraction actionMenuItemView2 = onView(
+        ViewInteraction actionMenuItemView4 = onView(
                 allOf(withId(R.id.favorited_menu), withContentDescription("Favorites"),
                         childAtPosition(
                                 childAtPosition(
@@ -194,22 +138,46 @@ public class PersistentDataDisplayTest {
                                         1),
                                 1),
                         isDisplayed()));
-        actionMenuItemView2.perform(click());
+        actionMenuItemView4.perform(click());
 
-        ViewInteraction recyclerView3 = onView(
+        ViewInteraction recyclerView4 = onView(
                 allOf(withId(R.id.favorite_recycler_view),
                         childAtPosition(
                                 withId(R.id.fragment_container),
                                 0)));
-        recyclerView3.perform(actionOnItemAtPosition(0, click()));
+        recyclerView4.perform(actionOnItemAtPosition(0, click()));
 
-        ViewInteraction editText2 = onView(
-                allOf(withId(R.id.instructions_edit_text), withText("wiggle"),
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.remove_from_favorites_button), withText("Remove"),
                         childAtPosition(
-                                withParent(withId(R.id.favorite_view_pager)),
-                                15),
+                                allOf(withId(R.id.linearLayout2),
+                                        childAtPosition(
+                                                withClassName(is("android.support.constraint.ConstraintLayout")),
+                                                14)),
+                                0),
                         isDisplayed()));
-        editText2.check(matches(withText("wiggle")));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton3.perform(click());
+
+        ViewInteraction linearLayoutCompat = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.action_bar),
+                                childAtPosition(
+                                        withId(R.id.action_bar_container),
+                                        0)),
+                        1),
+                        isDisplayed()));
+        linearLayoutCompat.check(matches(isDisplayed()));
 
     }
 
